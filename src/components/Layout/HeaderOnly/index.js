3c3fb65155cab61a1197c0b91b5f45f5a3fec875
Đức Long <duclongsew@gmail.com>
2022-06-07 10:33:21 +0700

    Add layout -Fix header -Add components layout -Add Header only -Check layout null

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,16 @@
+import React, { Children } from 'react'
+import Header from '../components/Header'
+
+
+const DefaultLayout = ({ children }) => {
+    return (
+        <div>
+            <Header />
+            <div className='container'>
+                <div className='content'>{children}</div>
+            </div>
+        </div>
+    )
+}
+
+export default DefaultLayout
