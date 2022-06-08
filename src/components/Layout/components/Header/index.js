a61cc3db932287d8f72d69735dd84c381bdf2893
Đức Long <duclongsew@gmail.com>
2022-06-08 10:58:20 +0700

    Add library Typpyjs -Custom modal -Add Popper global -Custom css modal search -Typpy interactive, visible -Add hook useState, useEffect check length search result -useEffect fake api -Setimer array useEffect

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
 import images from "../../../../assets/images";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import {
+    faCircleXmark,
+    faMagnifyingGlass,
+    faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
+
+//import show modal Tippy library
+import Tippy from "@tippyjs/react/headless";
+
+import { Wrapper as PopperWrapper } from "../../../Popper";
+
 const style = classNames.bind(styles);
 const Header = () => {
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            //fake api
+            setSearchResult([1, 2, 3]);
+        }, 0);
+    }, []);
     return (
         <header className={style("wrapper")}>
             <div className={style("inner")}>
                 <img src={images.logo} alt="Tiktok" />
-                <div className={style("search")}>
-                    <input placeholder="Search accounts and videos" spellCheck={false} />
-                    <button className={style("clear")}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <FontAwesomeIcon className={style('loading')} icon={faSpinner} />
-                    <button className={style("search-btn")}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                </div>
-                <div className={style('actions')}>
-                </div>
+                <Tippy
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={style("search-result")} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>Search result...</PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={style("search")}>
+                        <input
+                            placeholder="Search accounts and videos"
+                            spellCheck={false}
+                        />
+                        <button className={style("clear")}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={style("loading")} icon={faSpinner} />
+
+                        <button className={style("search-btn")}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={style("actions")}></div>
             </div>
         </header>
     );
